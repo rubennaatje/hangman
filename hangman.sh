094900a84c36a6b3987f95d08b4796099db81820
Ruben Soerdien <ruben.soerdien@hotmail.com>
2018-03-22 12:07:25 +0100

    added getopts

--- hangman.sh
+++ hangman.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+while getopts dw: opt
+do
+ case $opt in
+ d)
+ echo "random woord"
+ ;;
+ w)
+ echo "gekozen woord: $OPTARG"
+ ;;
+ \?)
+ echo "invalid option: -$OPTARG" >&2
+ exit
+ ;;
+ :)
+ echo "-$OPTARG needs parameter" >&2
+ exit
+ ;;
+ esac
+done
+
+echo "hangman";
